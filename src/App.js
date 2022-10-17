af86d4b8138de4622269d7c0509209e71833819b
Coding4usall <112630854+Coding4usall@users.noreply.github.com>
2022-10-17 14:05:51 -0300

    [PKD]0001 - Initial commit/Add Basic structure

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { React } from "react";
+//Routing
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+//Styles
+import { GlobalStyle } from "./GlobalStyle";
+//components
+import Home from "./Pages/Home";
+import PokeDetails from "./Pages/PokeDetails";
+
+
+const theme = {
+  colors: {
+    
+  },
+  mobile:{
+    firstBreakPoint: '1000px',
+    secondBreakPoint: '768px',
+    thirdBreakPoint: '600px',
+  }
+}
+
+
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Details/:name" element={<PokeDetails />} />
+      <Route
+        path="*"
+        element={
+          <main style={{ padding: "1rem" }}>
+            <p>There's nothing here!</p>
+          </main>
+        }
+      />
+    </Routes>
+    <GlobalStyle />
+  </Router>
+);
+
+export default App;
